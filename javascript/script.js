c77b4d85a370ed3ab6da7f179e68531da2c6444b
Modzie <modzielol@gmail.com>
2023-11-10 13:11:49 +0100

    add prompt to test a word

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+const word = "Bonjour";
+let yourWord = prompt("Please enter this word : " + word);
+
+if (yourWord === word)
+	console.log("Wunderbach!");
+else
+	console.log("Mistake was made, it happens");
